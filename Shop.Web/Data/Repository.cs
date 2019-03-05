6af23634aef2564f0dd958e79c318294c777e36d
María Alejandra G <alis990@hotmail.com>
2019-03-05 14:51:51 -0500

    Implement repository pattern

---
+++ Shop.Web/Data/Repository.cs
@@ -0,0 +1,56 @@
+﻿namespace Shop.Web.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Entities;
+
+    public class Repository : IRepository
+    {
+        private readonly DataContext context;
+
+        public Repository(DataContext context)
+        {
+            this.context = context;
+        }
+        //devuelve todos los productos ordenamos alfabeticamente por el campo
+        //nombre
+        public IEnumerable<Product> GetProducts()
+        {
+            return this.context.Products.OrderBy(p => p.Name);
+        }
+
+        //devuelve un objeto de la clase product
+        public Product GetProduct(int id)
+        {
+            return this.context.Products.Find(id);
+        }
+
+        public void AddProduct(Product product)
+        {
+            this.context.Products.Add(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            this.context.Update(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            this.context.Products.Remove(product);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await this.context.SaveChangesAsync() > 0;
+        }
+
+        public bool ProductExists(int id)
+        {
+            return this.context.Products.Any(p => p.Id == id);
+        }
+    }
+
+}
+
